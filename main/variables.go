4f495fcda68b17be862ff39b617a062c0618bbac
jackie <jackie.zheng@ximalaya.com>
2019-04-18 15:13:48 +0800

    initial go demo

---
+++ main/variables.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "initial"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	//var b, c = 1, 2
+	//b, c := 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "format"
+	fmt.Println(f)
+}
