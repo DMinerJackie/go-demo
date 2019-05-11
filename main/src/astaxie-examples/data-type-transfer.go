5916dbbceb7f57e04511b4cf84306de371c10844
Stark <zhengjie@bilibili.com>
2019-05-11 13:23:40 +0800

    add go demo - data-type-transfer

--- main/src/astaxie-examples/data-type-transfer.go
+++ main/src/astaxie-examples/data-type-transfer.go
@@ -21,4 +21,38 @@ func main() {
 	fmt.Println("valueText: ", valuesText)
 	fmt.Println("valueText: ", strings.Join(valuesText, "-"))
 
+	// int to string
+	i := 100
+	convertStr := strconv.Itoa(i)
+	fmt.Println("convertStr: ", convertStr)
+
+	// string to int
+	convertInt, err := strconv.Atoi(convertStr)
+	if err != nil {
+		fmt.Println("string to int convert error")
+	}
+	fmt.Println("convertInt: ", convertInt)
+
+	// string to int64
+	convertInt64, err := strconv.ParseInt(convertStr, 10, 64)
+	if err != nil {
+		fmt.Println("convertInt64: ", convertInt64)
+	}
+
+	// int64 to string
+	convertStr64 := strconv.FormatInt(convertInt64, 10)
+	fmt.Println("convertStr64: ", convertStr64)
+
+	convertFloat32, err := strconv.ParseFloat(convertStr, 32)
+	if err != nil {
+		fmt.Println("string to float32 error")
+	}
+	fmt.Println("convertFloat32: ", convertFloat32)
+
+	convertFloat64, err := strconv.ParseFloat(convertStr, 64)
+	if err != nil {
+		fmt.Println("string to float64 error")
+	}
+	fmt.Println("convertFloat64; ", convertFloat64)
+
 }
