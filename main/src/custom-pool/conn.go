94fa83a490014a6a06df249c9175a5ae264f0745
Stark <zhengjie@bilibili.com>
2019-09-08 10:19:17 +0800

    custom pool update

--- main/src/custom-pool/conn.go
+++ main/src/custom-pool/conn.go
@@ -9,8 +9,6 @@ import (
 )
 
 type Conn struct {
-	alive         bool
-	isExipre      bool
 	maxConn       int                     // 最大连接数
 	maxIdle       int                     // 最大可用连接数
 	freeConn      int                     // 线程池空闲连接数
@@ -19,7 +17,7 @@ type Conn struct {
 	waitConn      map[int]chan Permission // 排队等待的连接队列
 	waitCount     int                     // 等待个数
 	lock          sync.Mutex              // 锁
-	nextConnIndex int
+	nextConnIndex NextConnIndex
 	freeConns     map[int]Permission // 连接池的连接
 	expiredCh     chan string
 }
@@ -50,32 +48,29 @@ func Ping() {
 // TODO 1、使用channel归还连接 2、连接添加超时时间，如果超过超时时间没有拿到连接则退出
 func Prepare(ctx context.Context, config *Config) (conn *Conn) {
 	go func() {
-		for {
-			conn.expiredCh = make(chan string, len(conn.freeConns))
-			for _, value := range conn.freeConns {
-				if value.CreatedAt.Add(value.MaxLifeTime).Before(nowFunc()) {
-					conn.expiredCh <- "CLOSE"
-				}
-			}
-			//select {
-			//case <-conn.waitConn:
-			//	fmt.Println("receive")
-			//	conn.lock.Lock()
-			//	if conn.waitCount > 1 {
-			//		conn.waitCount--
-			//	}
-			//	conn.lock.Unlock()
-			//}
-		}
+		//for {
+		//conn.expiredCh = make(chan string, len(conn.freeConns))
+		//for _, value := range conn.freeConns {
+		//	if value.CreatedAt.Add(value.MaxLifeTime).Before(nowFunc()) {
+		//		conn.expiredCh <- "CLOSE"
+		//	}
+		//}
+		//select {
+		//case <-conn.waitConn:
+		//	fmt.Println("receive")
+		//	conn.lock.Lock()
+		//	if conn.waitCount > 1 {
+		//		conn.waitCount--
+		//	}
+		//	conn.lock.Unlock()
+		//}
+		//}
 
 	}()
 	return &Conn{
-		alive:     false,
-		isExipre:  false,
 		maxConn:   config.MaxConn,
 		maxIdle:   config.MaxIdle,
 		openCount: 0,
-		freeConn:  0,
 		connPool:  []int{},
 		waitConn:  make(map[int]chan Permission),
 		waitCount: 0,
@@ -86,35 +81,36 @@ func Prepare(ctx context.Context, config *Config) (conn *Conn) {
 // 创建连接
 func (conn *Conn) New(ctx context.Context) (permission Permission, err error) {
 	/**
-	1、如果当前连接池已满，即freeConn=0
+	1、如果当前连接池已满，即len(freeConns)=0
 	2、判定openConn是否大于maxConn，如果大于，则丢弃获取加入队列进行等待
 	3、如果小于，则考虑创建新连接
 	注意设置超时时间以及用完连接的回收
-
-	任务满了就加入等待队列，什么时候监听消息
 	*/
 	conn.lock.Lock()
 
 	select {
 	default:
 	case <-ctx.Done():
-		nextConnIndex := getNextConnIndex(conn)
 		conn.lock.Unlock()
 
-		return Permission{NextConnIndex: NextConnIndex{nextConnIndex},
-				Content: "PASSED", CreatedAt: nowFunc(), MaxLifeTime: time.Second * 5},
-			errors.New("new conn failed, context cancelled!")
+		return Permission{}, errors.New("new conn failed, context cancelled!")
 	}
 
-	//if conn.freeConn > 0 {
-	//	conn.connPool = conn.connPool[1:] // 取出一个连接
-	//	conn.freeConn--
-	//	fmt.Println("openCount: ", conn.openCount, " freeConn: ", conn.freeConn, " connPool: ", conn.connPool)
-	//	conn.lock.Unlock()
-	//	return true, nil
-	//}
+	if len(conn.freeConns) > 0 {
+		var (
+			popPermission Permission
+			popReqKey     int
+		)
+		for popReqKey, popPermission = range conn.freeConns {
+			break
+		}
+		delete(conn.freeConns, popReqKey)
+		fmt.Println("log", "use free conn!!!!!", "openCount: ", conn.openCount, " freeConns: ", conn.freeConns)
+		conn.lock.Unlock()
+		return popPermission, nil
+	}
 
-	if conn.openCount > conn.maxConn { // 当前连接数大于上限，则加入等待队列
+	if conn.openCount >= conn.maxConn { // 当前连接数大于上限，则加入等待队列
 		//conn.lock.Lock()
 		//conn.waitConn <- 1
 		nextConnIndex := getNextConnIndex(conn)
@@ -126,52 +122,46 @@ func (conn *Conn) New(ctx context.Context) (permission Permission, err error) {
 		conn.lock.Unlock()
 
 		select {
-		case <-conn.expiredCh:
-			return Permission{}, errors.New("new conn failed, timeout")
+		case <-time.After(time.Second * time.Duration(3)):
+			fmt.Println("超时，通知主线程退出")
+			return
 		case ret, ok := <-req: // 有放回的连接, 直接拿来用
-			fmt.Println("received release conn !!!")
 			if !ok {
 				return Permission{}, errors.New("new conn failed, no available conn release")
 			}
-			conn.lock.Lock()
-			//delete(conn.waitConn, )
-			conn.openCount++
-			conn.lock.Unlock()
+			fmt.Println("log", "received released conn!!!!!", "openCount: ", conn.openCount, " freeConns: ", conn.freeConns)
 			return ret, nil
 		}
 		return Permission{}, errors.New("new conn failed")
 	}
 
 	// 新建连接
-	conn.connPool = append(conn.connPool, 1)
 	conn.openCount++
-	conn.freeConn++
 	conn.lock.Unlock()
 	nextConnIndex := getNextConnIndex(conn)
 	permission = Permission{NextConnIndex: NextConnIndex{nextConnIndex},
 		Content: "PASSED", CreatedAt: nowFunc(), MaxLifeTime: time.Second * 5}
-	conn.freeConns[nextConnIndex] = permission
-	fmt.Println("openCount: ", conn.openCount, " freeConn: ", conn.freeConn, " connPool: ", conn.connPool)
+	//conn.freeConns[nextConnIndex] = permission
+	fmt.Println("log", "create conn!!!!!", "openCount: ", conn.openCount, " freeConns: ", conn.freeConns)
 	return permission, nil
 }
 
 func getNextConnIndex(conn *Conn) int {
-	currentIndex := conn.nextConnIndex
+	currentIndex := conn.nextConnIndex.Index
 	//conn.lock.Lock()
-	conn.nextConnIndex = currentIndex + 1
-	fmt.Println("connIndex: ", conn.nextConnIndex)
+	conn.nextConnIndex.Index = currentIndex + 1
+	//fmt.Println("connIndex: ", conn.nextConnIndex)
 	//conn.lock.Lock()
-	return conn.nextConnIndex
+	return conn.nextConnIndex.Index
 }
 
 // 释放连接
 func (conn *Conn) Release(ctx context.Context) (result bool, err error) {
 	conn.lock.Lock()
-	conn.openCount--
-	conn.connPool = append(conn.connPool, 1)
-	conn.freeConn++
-	conn.lock.Unlock()
-	fmt.Println("openCount: ", conn.openCount, " freeConn: ", conn.freeConn, " connPool: ", conn.connPool)
+	//conn.openCount--
+	//conn.freeConn++
+	//conn.lock.Unlock()
+	//fmt.Println("openCount: ", conn.openCount, " freeConns: ", conn.freeConns)
 
 	if len(conn.waitConn) > 0 {
 		var req chan Permission
@@ -183,10 +173,19 @@ func (conn *Conn) Release(ctx context.Context) (result bool, err error) {
 		permission := Permission{NextConnIndex: NextConnIndex{reqKey},
 			Content: "PASSED", CreatedAt: nowFunc(), MaxLifeTime: time.Second * 5}
 		req <- permission
-		conn.freeConns[reqKey] = permission
-		conn.lock.Lock()
+		//conn.freeConns[reqKey] = permission
+		//conn.lock.Lock()
 		conn.waitCount--
 		delete(conn.waitConn, reqKey)
+		conn.lock.Unlock()
+	} else {
+		if conn.openCount > 0 {
+			conn.openCount--
+			nextConnIndex := getNextConnIndex(conn)
+			permission := Permission{NextConnIndex: NextConnIndex{nextConnIndex},
+				Content: "PASSED", CreatedAt: nowFunc(), MaxLifeTime: time.Second * 5}
+			conn.freeConns[nextConnIndex] = permission
+		}
 		conn.lock.Unlock()
 	}
 	return
--- main/src/custom-pool/start/main.go
+++ main/src/custom-pool/start/main.go
@@ -9,21 +9,14 @@ func main() {
 
 	ctx := context.Background()
 	config := &custom_pool.Config{
-		MaxConn: 1,
+		MaxConn: 2,
 		MaxIdle: 1,
 	}
 	conn := custom_pool.Prepare(ctx, config)
 	if _, err := conn.New(ctx); err != nil {
 		return
 	}
-	if _, err := conn.New(ctx); err != nil {
-		return
-	}
 	//go conn.Release(ctx)
-	if _, err := conn.New(ctx); err != nil {
-		return
-	}
-	go conn.Release(ctx)
 	if _, err := conn.New(ctx); err != nil {
 		return
 	}
